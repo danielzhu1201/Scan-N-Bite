b07aa7ca146dae08376512762387df3fae6e3235
Mengrui Luo <55298448+MengruiLuo@users.noreply.github.com>
2020-06-24 23:45:25 -0500

    finished categort mene page ui

--- src/components/FoodCategory/index.tsx
+++ src/components/FoodCategory/index.tsx
@@ -2,11 +2,18 @@ import React, {useState} from "react";
 
 import './style.css'
 
+const imgURL = '/popeyes.jpg'
 
 const FoodCategory: React.FC = () => {
+  const [category, setCategory] = useState('Food Type');
+  const [description, setDescription] = useState('Descriptions here and they wrap!!');
+  const [imageURL, setImageURL] = useState(imgURL);
 
   return (
     <div className="foodCatogory">
+      <img className="foodCategory__img" src={process.env.PUBLIC_URL + imageURL} />
+      <p className='foodCategory__categoryName'>{category}</p>
+      <p className='foodCategory__categoryDesc'>{description}</p>
     </div>
   )
 }
--- src/containers/menu/category/index.tsx
+++ src/containers/menu/category/index.tsx
@@ -3,16 +3,28 @@ import React from "react";
 import Headline from '../../../components/Headline'
 import FoodCategory from '../../../components/FoodCategory'
 
+import './style.css'
+
 const MenuCategory: React.FC = () => {
   return (
-    <div className = 'menuCategory'>
-      <Headline />
-      <div>
-        <FoodCategory />
-        <FoodCategory />
-        <FoodCategory />
-        <FoodCategory />
-      </div>
+    <div className='menuCategory'>
+        <div>
+          <Headline />
+        </div>
+        <div className='menuCategory__categoryWrapper'>
+        <div className='menuCategory__category'>
+            <FoodCategory />
+          </div>
+          <div className='menuCategory__category'>
+            <FoodCategory />
+          </div>
+          <div className='menuCategory__category'>
+            <FoodCategory />
+          </div>
+          <div className='menuCategory__category'>
+            <FoodCategory />
+          </div>
+        </div>
     </div>
   )
 }
