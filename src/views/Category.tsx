1317fad53d60a22822d778f6582c9af034a09af6
Mengrui Luo <55298448+MengruiLuo@users.noreply.github.com>
2020-06-26 17:47:03 -0500

    fixed the margin for menu page

--- src/views/Category.tsx
+++ src/views/Category.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import Headline from '../components/Headline'
 import FoodCategory from '../components/FoodCategory'
@@ -6,31 +7,60 @@ import FoodCategory from '../components/FoodCategory'
 import modules from "../components/styles/Category.module.css"
 
 const Category: React.FC = () => {
+  const matches = useMediaQuery('(min-width:360px)');
+  console.log(matches)
   return (
     <div className={modules.MenuCategory}>
       <div>
         <Headline />
       </div>
-      <div className={modules.MenuCategoryCategoryWrapper}>
-        <div className={modules.MenuCategoryCategory}>
-          <FoodCategory />
-        </div>
-        <div className={modules.MenuCategoryCategory}>
-          <FoodCategory />
-        </div>
-        <div className={modules.MenuCategoryCategory}>
-          <FoodCategory />
-        </div>
-        <div className={modules.MenuCategoryCategory}>
-          <FoodCategory />
-        </div>
-        <div className={modules.MenuCategoryCategory}>
-          <FoodCategory />
-        </div>
-        <div className={modules.MenuCategoryCategory}>
-          <FoodCategory />
-        </div>
-      </div>
+        {!matches ? 
+          (
+            <div className={modules.MenuCategoryCategoryWrapper}>
+              <div className={modules.MenuCategoryCategory}>
+                <FoodCategory />
+              </div>
+              <div className={modules.MenuCategoryCategory}>
+                <FoodCategory />
+              </div>
+              <div className={modules.MenuCategoryCategory}>
+                <FoodCategory />
+              </div>
+              <div className={modules.MenuCategoryCategory}>
+                <FoodCategory />
+              </div>
+              <div className={modules.MenuCategoryCategory}>
+                <FoodCategory />
+              </div>
+              <div className={modules.MenuCategoryCategory}>
+                <FoodCategory />
+              </div>
+            </div>
+          )
+          :
+          (
+            <div className={modules.MenuCategoryCategoryWrapper}>
+              <div className={modules.menuCategory__category__left}>
+                <FoodCategory />
+              </div>
+              <div className={modules.menuCategory__category__right}>
+                <FoodCategory />
+              </div>
+              <div className={modules.menuCategory__category__left}>
+                <FoodCategory />
+              </div>
+              <div className={modules.menuCategory__category__right}>
+                <FoodCategory />
+              </div>
+              <div className={modules.menuCategory__category__left}>
+                <FoodCategory />
+              </div>
+              <div className={modules.menuCategory__category__right}>
+                <FoodCategory />
+              </div>
+            </div>
+          )
+        }
     </div>
   )
 }
