6033624c2a1a65477dee561f7452203ab6f5e10c
Zhaosong Zhu <zhaosong.zhu@utexas.edu>
2020-06-29 13:50:45 -0500

    Update NotFoundPage.tsx

--- src/NotFoundPage.tsx
+++ src/NotFoundPage.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-
 function NotFoundPage() {
   return (
-    <div style={{
-      position: "relative",
-      width: "100 %",
-      height: "159px",
-      left: "0px",
-      top: "50px"
-    }}>
-      <h1>You are in the deserts now!</h1>;
+    <div
+      style={{
+        position: "relative",
+        width: "100 %",
+        height: "159px",
+        left: "0px",
+        top: "50px",
+      }}
+    >
+      <h1>Profile Page Coming Soon</h1>
     </div>
   );
 }
