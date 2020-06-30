a821f9844aa9f91deb3bc4e7f22a64f00a1aea11
Mengrui Luo <55298448+MengruiLuo@users.noreply.github.com>
2020-06-30 13:12:50 -0500

    fix version problem

--- src/components/OrderButton.tsx
+++ src/components/OrderButton.tsx
@@ -5,6 +5,11 @@ import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 import Button from "@material-ui/core/Button";
 
 import modules from "./styles/OrderButton.module.css";
+import {
+  ToastsContainer,
+  ToastsStore,
+  ToastsContainerPosition,
+} from "react-toasts";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -34,6 +39,7 @@ const OrderButton: React.FC<OrderButtonProps> = ({ price }) => {
   return (
     <div className={modules.OrderButton}>
       <Button
+        onClick={() => ToastsStore.success("Added Sucessful!")}
         variant="contained"
         color="primary"
         startIcon={<ShoppingCartOutlinedIcon />}
@@ -44,6 +50,11 @@ const OrderButton: React.FC<OrderButtonProps> = ({ price }) => {
       >
         {price}
       </Button>
+      <ToastsContainer
+        store={ToastsStore}
+        position={ToastsContainerPosition.TOP_CENTER}
+        lightBackground={true}
+      />
     </div>
   );
 };
