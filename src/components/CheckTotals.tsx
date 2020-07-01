d69c581ee8347a8e77e3bbb1638b5b55e275e71c
Zhaosong Zhu <zhaosong.zhu@utexas.edu>
2020-07-01 13:49:32 -0500

    Check total no more nan

--- src/components/CheckTotals.tsx
+++ src/components/CheckTotals.tsx
@@ -1,11 +1,11 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 
 import modules from "./styles/CheckTotals.module.css";
 
 export default function CheckTotals() {
-    const [checkSubtotal, setCheckSubtotal] = useState(100);
-    const [tipTotal, setTipTotal] = useState(0);
-    const [checkTotal, setCheckTotal] = useState(checkSubtotal+tipTotal);
+  const [checkSubtotal, setCheckSubtotal] = useState(100);
+  const [tipTotal, setTipTotal] = useState(0);
+  const [checkTotal, setCheckTotal] = useState(checkSubtotal + tipTotal);
 
     function updateBill() {
         setCheckTotal(checkSubtotal + tipTotal)
@@ -13,30 +13,59 @@ export default function CheckTotals() {
     }
   return (
     <div className={modules.SubTotal}>
-        <hr className={modules.break}/>
-        <div className={modules.checkStub}>
-            <div className={modules.Totals}>
-                <div className={modules.checkTotal}>SubTotal</div>
-                    <div className={modules.checkAmount} id="subTotal">$  {checkSubtotal}</div>
-            </div> 
-            <div className={modules.Totals}>
-                <div className={modules.checkTotal}>Tip</div>
-                <div className={modules.tipInput}>
-                    $ <input type="number" id="tipBox"className={modules.Tip} value={tipTotal} onChange={(e)=> setTipTotal(parseFloat(e.target.value))}/>
-                </div>
-            </div>
-            <div className={modules.tipButtons}>
-                <div className={modules.container}>
-                    <button className={modules.button} onClick={(e) => setTipTotal(checkSubtotal*.15)}>15%</button>
-                    <button className={modules.button} onClick={(e) => setTipTotal(checkSubtotal*.20)}>20%</button>
-                    <button className={modules.button} onClick={(e) => setTipTotal(checkSubtotal*.25)}>25%</button>
-                </div>
-            </div> 
-            <div className={modules.Totals}>
-                <div className={modules.checkTotal}>Total</div>
-                <div className={modules.checkAmount}>$ {checkSubtotal + tipTotal}</div>
-            </div>
+      <hr className={modules.break} />
+      <div className={modules.checkStub}>
+        <div className={modules.Totals}>
+          <div className={modules.checkTotal}>SubTotal</div>
+          <div className={modules.checkAmount} id="subTotal">
+            $ {checkSubtotal}
+          </div>
         </div>
+        <div className={modules.Totals}>
+          <div className={modules.checkTotal}>Tip</div>
+          <div className={modules.tipInput}>
+            ${" "}
+            <input
+              type="number"
+              id="tipBox"
+              className={modules.Tip}
+              value={tipTotal}
+              onChange={(e) => setTipTotal(parseFloat(e.target.value))}
+            />
+          </div>
+        </div>
+        <div className={modules.tipButtons}>
+          <div className={modules.container}>
+            <button
+              className={modules.button}
+              onClick={(e) => setTipTotal(checkSubtotal * 0.15)}
+            >
+              15%
+            </button>
+            <button
+              className={modules.button}
+              onClick={(e) => setTipTotal(checkSubtotal * 0.2)}
+            >
+              20%
+            </button>
+            <button
+              className={modules.button}
+              onClick={(e) => setTipTotal(checkSubtotal * 0.25)}
+            >
+              25%
+            </button>
+          </div>
+        </div>
+        <div className={modules.Totals}>
+          <div className={modules.checkTotal}>Total</div>
+          <div className={modules.checkAmount}>
+            ${" "}
+            {Number.isNaN(checkSubtotal + tipTotal)
+              ? "..."
+              : checkSubtotal + tipTotal}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
