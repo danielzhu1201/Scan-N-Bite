6e7d7942db08b49cedf83fe4847e5428a08844d9
Zhaosong Zhu <zhaosong.zhu@utexas.edu>
2020-07-01 22:01:00 -0500

    Made a few updates to the modals

--- src/components/CreditCardModal.tsx
+++ src/components/CreditCardModal.tsx
@@ -1,40 +1,68 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
 import exit from "../exit.svg";
-import checkmark from "../checkmark.svg";
 
-import modules from "./styles/CreditCardModal.module.css"
-const waitModal = ({isShowing, hide}) => isShowing ? ReactDom.createPortal(
-    <React.Fragment>
-        <div className={modules.modal}>
+import modules from "./styles/CreditCardModal.module.css";
+const waitModal = ({ isShowing, hide }) =>
+  isShowing
+    ? ReactDom.createPortal(
+        <React.Fragment>
+          <div className={modules.modal}>
             <div className={modules.container}>
-                <div className={modules.modalClose}>
-            <img src={exit} className={modules.exit} onClick={hide} alt="TypePic"/>
-            </div>
-            <form className={modules.CreditCardForm}>
+              <div className={modules.modalClose}>
+                <img
+                  src={exit}
+                  className={modules.exit}
+                  onClick={hide}
+                  alt="TypePic"
+                />
+              </div>
+              <form className={modules.CreditCardForm}>
                 <div className={modules.InputWrapper}>
-                    <input type="text" className={modules.Name} value="" placeholder="Name"/>
+                  <input
+                    type="text"
+                    className={modules.Name}
+                    value=""
+                    placeholder="Name"
+                  />
                 </div>
                 <div className={modules.InputWrapper}>
-                    <input type="number" className = {modules.Name} placeholder="Credit Card Number"/>
+                  <input
+                    type="number"
+                    className={modules.Name}
+                    placeholder="Credit Card Number"
+                  />
                 </div>
                 <div className={modules.ExpirationDate}>
-                    <input type="number" className={modules.CreditCardExpirationDate} placeholder="Expiration Month"/>
-                    <div className={modules.Slash}>/</div>
-                    <input type="number" className={modules.CreditCardExpirationDate} placeholder="Expiration Year"/>   
+                  <input
+                    type="number"
+                    className={modules.CreditCardExpirationDate}
+                    placeholder="Expiration Month"
+                  />
+                  <div className={modules.Slash}>/</div>
+                  <input
+                    type="number"
+                    className={modules.CreditCardExpirationDate}
+                    placeholder="Expiration Year"
+                  />
                 </div>
                 <div className={modules.InputWrapper}>
-                    <input type="number" className={modules.Name} placeholder="CVV"/>
+                  <input
+                    type="number"
+                    className={modules.Name}
+                    placeholder="CVV"
+                  />
                 </div>
                 <div className={modules.InputWrapper}>
-                    <button className={modules.CCPay}>Pay</button>    
+                  <button className={modules.CCPay}>Pay</button>
                 </div>
-                
-                </form>
-        </div>
-    </div>
-    </React.Fragment>, document.body
-) : null;
+              </form>
+            </div>
+          </div>
+        </React.Fragment>,
+        document.body
+      )
+    : null;
 
-export default waitModal
+export default waitModal;
--- src/views/Pay.tsx
+++ src/views/Pay.tsx
@@ -1,20 +1,81 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, Redirect } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import Divider from "@material-ui/core/Divider";
+import CreditCardButton from "../components/CreditCardButton";
+import CashButton from "../components/cashButton";
 
 import modules from "./styles/Pay.module.css";
 
 class Pay extends React.Component<any, any> {
   constructor(props) {
     super(props);
+    this.state = {
+      paymentSuccess: false,
+    };
   }
 
   render() {
     const info = this.props.location.paymentProps;
-    console.log(info);
+
     return (
-      <div className={modules.Container}>
-        {info ? <div>{`My payment info is ${info.amount}`}</div> : <div></div>}
-      </div>
+      <React.Fragment>
+        {true ? (
+          <div className={modules.Container}>
+            <div className={modules.Total}>Your total is</div>
+            <div className={modules.TotalNum}>{`$${10}`}</div>
+            <Divider style={{ width: "80%", margin: "10px 0px" }} />
+            <div className={modules.CheckoutText}>
+              How would you like to checkout?
+            </div>
+            <Helmet>
+              <script type="text/javascript">{`var amount = ${10};`}</script>
+              <script type="text/javascript">{`console.log(amount)`}</script>
+              <script
+                type="text/javascript"
+                src="https://sandbox-assets.secure.checkout.visa.com/checkout-widget/resources/js/integration/v1/sdk.js"
+              ></script>
+              <script type="text/javascript">
+                {`function onVisaCheckoutReady() {
+                    V.init({
+                    apikey: "PQY61HUOVKXW1E1AITP621_jPkgtZlzt9320UI02Ergl9PF5g",
+                    encryptionKey: "W9O4GYP5A4UOXGU2RR4513FZ-EpKghPT6dT1QKiVvNFCzO5Ms",
+                    payment: {
+                        cardBrands: ["VISA", "MASTERCARD", "AMEX", "DINERS", "DISCOVER"],
+                    },
+                    paymentRequest: {
+                        currencyCode: "USD",
+                        subtotal: amount,
+                    },
+                    dataLevel: "NONE",
+                    });
+                    V.on("payment.success", function (payment) {
+                    localStorage.setItem("status", "Success");
+                    
+                    });
+                    V.on("payment.cancel", function (payment) {
+                    localStorage.setItem("status", "Cancel");
+                    });
+                    V.on("payment.error", function (payment, error) {
+                    localStorage.setItem("status", "Fail");
+                    });
+                }`}
+              </script>
+            </Helmet>
+            <img
+              alt="Visa Checkout"
+              className="v-button"
+              role="button"
+              src="https://sandbox.secure.checkout.visa.com/wallet-services-web/xo/button.png"
+            />
+            <CreditCardButton />
+            <CashButton />
+            <div>{this.state.paymentSuccess}</div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </React.Fragment>
     );
   }
 }
